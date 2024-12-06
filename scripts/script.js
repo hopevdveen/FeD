3b3d47478aa54dc442361163f249afa8c87398a5
hopevdveen <145197613+hopevdveen@users.noreply.github.com>
2024-12-06 14:28:13 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
-// JavaScript Document
-console.log("hi");
+var menuButton = document.querySelector("header nav img:nth-of-type(3)");
+var hetMenu = document.querySelector("header nav ul");
+var deNav = document.querySelector("header nav")
+
+menuButton.addEventListener("click", openMenu)
+
+function openMenu(){
+    hetMenu.classList.toggle("toonMenu");
+    deNav.classList.add("veranderKleur");
+    menuButton.src = "images/cross.png";
+}
+
